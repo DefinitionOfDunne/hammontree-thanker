9cb44f21919788eeeafd9999f7b185f961aa5103
Dylan Dunne <dylanadunne@gmail.com>
2016-08-07 14:19:44 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*--- Display information modal box ---*/
+  	$("#message-revealer").click(function(){
+    $(".overlay").fadeIn(1000);
+	});
+
+ /*--- Hide information modal box ---*/
+  	$("#hide-message").click(function(){
+  	$(".overlay").fadeOut(1000);
+ 	});
