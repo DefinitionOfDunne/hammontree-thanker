01eb39046f60df1b3286fdce8f7263584403282a
Dylan Dunne <dylanadunne@gmail.com>
2016-08-07 17:40:16 -0400

    Add files via upload
    file updates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,70 @@
 /*--- Display information modal box ---*/
-  	$("#message-revealer").click(function(){
-    $(".overlay").fadeIn(1000);
-	});
+$("#scott-circle").click(function() {
+    $("#scott-message").fadeIn(1000);
+});
 
- /*--- Hide information modal box ---*/
-  	$("#hide-message").click(function(){
-  	$(".overlay").fadeOut(1000);
- 	});
+/*--- Hide information modal box ---*/
+$(".hide-message").click(function() {
+    $("#scott-message").fadeOut(1000);
+});
+
+/*--- Display information modal box ---*/
+$("#lynn-circle").click(function() {
+    $("#lynn-message").fadeIn(1000);
+});
+
+/*--- Hide information modal box ---*/
+$(".hide-message").click(function() {
+    $("#lynn-message").fadeOut(1000);
+});
+
+/*--- Display information modal box ---*/
+$("#holden-circle").click(function() {
+    $("#holden-message").fadeIn(1000);
+});
+
+/*--- Hide information modal box ---*/
+$(".hide-message").click(function() {
+    $("#holden-message").fadeOut(1000);
+});
+
+/*--- Display information modal box ---*/
+$("#claire-circle").click(function() {
+    $("#claire-message").fadeIn(1000);
+});
+
+/*--- Hide information modal box ---*/
+$(".hide-message").click(function() {
+    $("#claire-message").fadeOut(1000);
+});
+
+
+
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '270',
+        width: '480',
+        videoId: 'W9n6yhmqyF0',
+        events: {
+        }
+    });
+}
+
+
+
+
+
+function stopVideo() {
+    player.stopVideo();
+}
